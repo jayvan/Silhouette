c27b2bdd92c46d34c2ff43e917b3dbbb59418bca
Justin Vanderheide <justin@justinvanderheide.com>
2012-05-10 18:50:22 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Silhouette::Application.config.secret_token = '3ca7e8731939ef04514de76f5294e8e9b44d40543865e07450bfdf73bdf0be4f7bb2b6afeb2fb4b4408287d7874f5f8ca5c747f1a0aacb4c11bb9e2fe34a8e4b'
---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20120510222256) do
+
+  create_table "people", :force => true do |t|
+    t.text    "first_name"
+    t.text    "last_name"
+    t.integer "practical_score"
+    t.integer "written_score"
+    t.integer "communication_score"
+  end
+
+end
